c658690c34776fd1b0146b91adc06b7041717900
ali <ali.assar@skiff.com>
2024-05-23 22:36:14 +0330

    feat: make work telegram api and open Weather Map api

--- main.go
+++ main.go
@@ -1,32 +1,81 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 
-	"github.com/Ali-Assar/SkySpyBot/handler"
+	"github.com/Ali-Assar/SkySpyBot/types"
 	owm "github.com/briandowns/openweathermap"
 	"github.com/joho/godotenv"
 )
 
-func main() {
-	if err := godotenv.Load(); err != nil {
-		log.Fatal("Error loading .env file:", err)
-	}
-	apiKey := os.Getenv("OWM_API_KEY")
+var (
+	OWMApiKey      string
+	TelegramApikey string
+)
 
-	w, err := owm.NewCurrent("C", "EN", apiKey)
+func Handler(res http.ResponseWriter, req *http.Request) {
+
+	body := &types.WebhookReqBody{}
+	if err := json.NewDecoder(req.Body).Decode(body); err != nil {
+		fmt.Println("could not decode request body", err)
+		return
+	}
+
+	if err := SendLocation(body.Message.Chat.ID, body.Message.Text); err != nil {
+		fmt.Println("error in sending reply:", err)
+		return
+	}
+
+	// log a confirmation message if the message is sent successfully
+	fmt.Println("reply sent")
+}
+
+func SendLocation(chatID int64, text string) error {
+
+	w, err := owm.NewCurrent("C", "EN", OWMApiKey)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	w.CurrentByName("mashhad")
+	w.CurrentByName(text)
+	// Create the request body struct
+	reqBody := &types.SendMessageReqBody{
+		ChatID: chatID,
+		Text:   fmt.Sprintf("temp= %v", int(w.Main.Temp)),
+	}
+	// Create the JSON body from the struct
+	reqBytes, err := json.Marshal(reqBody)
+	if err != nil {
+		return err
+	}
 
-	fmt.Println(w.Weather)
-	fmt.Println(w.Main.Temp)
+	telegramApi := fmt.Sprintf("https://api.telegram.org/bot%s/sendMessage", TelegramApikey)
+	// Send a post request with your token
+	res, err := http.Post(telegramApi, "application/json", bytes.NewBuffer(reqBytes))
+	if err != nil {
+		return err
+	}
+	if res.StatusCode != http.StatusOK {
+		return errors.New("unexpected status" + res.Status)
+	}
 
-	http.HandleFunc("/webhook", handler.HandleTelegramWebHook)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	return nil
+}
+
+// FInally, the main funtion starts our server on port 3000
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file:", err)
+	}
+
+	OWMApiKey = os.Getenv("OWM_API_KEY")
+	TelegramApikey = os.Getenv("TELEGRAM_BOT_TOKEN")
+	log.Println("running")
+	http.ListenAndServe(":8080", http.HandlerFunc(Handler))
 }
--- types/types.go
+++ types/types.go
@@ -1,18 +1,15 @@
 package types
 
-// Update is a Telegram object that the handler receives every time an user interacts with the bot.
-type Update struct {
-	UpdateId int     `json:"update_id"`
-	Message  Message `json:"message"`
+type WebhookReqBody struct {
+	Message struct {
+		Text string `json:"text"`
+		Chat struct {
+			ID int64 `json:"id"`
+		} `json:"chat"`
+	} `json:"message"`
 }
 
-// Message is a Telegram object that can be found in an update.
-type Message struct {
-	Text string `json:"text"`
-	Chat Chat   `json:"chat"`
-}
-
-// A Telegram Chat indicates the conversation to which the message belongs.
-type Chat struct {
-	Id int `json:"id"`
+type SendMessageReqBody struct {
+	ChatID int64  `json:"chat_id"`
+	Text   string `json:"text"`
 }
